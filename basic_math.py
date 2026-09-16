0c1c2e70b3dc0fa7b40d5b9bf435ceb66bbfb5e3
Emmanuella Taiwo <emmanuella.taiwo@stu.ocsb.ca>
2026-09-16 03:35:48 +0000

    i added simple math calculations to the code

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Created By: Emmanuella Taiwo
+# Date: Sept 15th, 2026
+# This program does basic math
+
+
+def main():
+    print("3-5={}".format(3 - 5))
+    print("7+3={}".format(7 + 3))
+    print("8*6={}".format(8 * 6))
+    print("4/2={}".format(4 / 2))
+    print("1/2={}".format(1 / 2))
+    print("2+5*3={}".format(2 + 5 * 3))
+    print("2**3={}".format(2**3))  # exponent
+    print("16**0.5={}".format(16**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
